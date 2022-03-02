e4722ba3eb8c117d8284fbad0c0b90e9114e0fb4
Abhishek Jain <abhishek.j0407@gmail.com>
2022-03-02 22:44:59 +0530

    goals routes created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('hello bhai')
+//get dependencies
+
+const { urlencoded } = require('express')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000     //read port from .env file, or set it to 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))  //specify the route to use(look in) goalRoutes file
+
+app.listen(port, () => console.log(`Server started at port ${port}`))
+
