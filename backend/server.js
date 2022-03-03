fc4bf49296043cafe2b807439ada72e4b7dbb97b
Abhishek Jain <abhishek.j0407@gmail.com>
2022-03-03 20:05:21 +0530

    error handler added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 const { urlencoded } = require('express')
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000     //read port from .env file, or set it to 5000
 
 const app = express()
@@ -11,6 +12,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))  //specify the route to use(look in) goalRoutes file
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started at port ${port}`))
 
